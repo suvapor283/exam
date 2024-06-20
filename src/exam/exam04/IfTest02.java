c93bee725cb4b0747e59b0e87ef36e72ce282ff8
suvapor283 <suvapor8632@gmail.com>
2024-06-20 21:45:17 +0900

    exam 04

---
+++ src/exam/exam04/IfTest02.java
@@ -0,0 +1,11 @@
+package exam.exam04;
+
+import java.util.Scanner; // 자동 import: ctrl + shift + o
+
+public class IfTest02 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); 
+	}
+}
